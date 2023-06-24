258d640237c08685b999a2ac48d2b0d1b0e468aa
Mitsufiro <lyashenk.ilya@gmail.com>
2023-06-24 19:50:45 +0300

    Load project

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi_sqlalchemy import DBSessionMiddleware
 from routers import auth, flowers, orders, flowersinorder
 
 load_dotenv(".env")
-app = FastAPI(title="DataGO Service", version="0.0.1")
+app = FastAPI(title="DataGO Flowers Service", version="0.0.1")
 app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 app.include_router(auth.ROUTER)
 app.include_router(flowers.ROUTER)
